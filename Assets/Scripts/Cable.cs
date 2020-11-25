1899c7086cfc4bec4a8bba0bb7c6347d8d8e6257
Spazi <talishmarprkle+g@gmail.com>
2020-11-25 10:20:04 +0200

    ClawTesting

---
+++ Assets/Scripts/Cable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cable : MonoBehaviour
+{
+    [SerializeField]
+    GameObject claw;
+    [SerializeField]
+    int playerNumber = 1;
+    int lengthOfLineRenderer = 2;
+    
+    GameObject[] cablePoints;
+
+    void Start()
+    {
+        if (playerNumber == 1)
+        {
+            if (cablePoints == null)
+            {
+                cablePoints = GameObject.FindGameObjectsWithTag("Player1Cables");
+            }
+        }
+        if (playerNumber == 2)
+        {
+            if (cablePoints == null)
+            {
+                cablePoints = GameObject.FindGameObjectsWithTag("Player2Cables");
+            }
+        }
+
+        LineRenderer cable = gameObject.GetComponent<LineRenderer>();
+        cable.positionCount = lengthOfLineRenderer;
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LineRenderer cable = gameObject.GetComponent<LineRenderer>();
+        var points = new Vector3[lengthOfLineRenderer];
+        for (int i = 0; i < lengthOfLineRenderer; i++)
+        {
+            Vector3 pos = cablePoints[i].transform.position;
+            points[i] = pos;
+        }
+        cable.SetPositions(points);
+    } 
+}
---
+++ Assets/Scripts/ClawController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawController : MonoBehaviour
+{
+    bool clawOpen = false;
+    Animator anim;
+    float currentClawWeight;
+    float newClawWeight;
+    float clawWeightTarget;
+    float clawVelocity; 
+    float smoothTime = 0.1f; 
+
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        currentClawWeight = anim.GetLayerWeight(1);
+
+        if (Input.GetButton("Fire1") && (!clawOpen))
+        {
+            
+            //anim.SetLayerWeight(1, newClawWeight);
+            clawWeightTarget = 1f;
+            clawOpen = true;
+        }
+
+        if (!Input.GetButton("Fire1") && (clawOpen))
+        {
+            //anim.SetLayerWeight(1, newClawWeight);
+            clawWeightTarget = 0f;
+            clawOpen = false;
+        }
+        MoveClaw();
+    }
+    
+    void MoveClaw()
+    {
+        newClawWeight = Mathf.SmoothDamp(currentClawWeight, clawWeightTarget, ref clawVelocity, smoothTime);
+        anim.SetLayerWeight(1, newClawWeight);
+    }
+}
